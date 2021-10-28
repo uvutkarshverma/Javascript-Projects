a239e353f38d09f724e205326d5922d79031fc3d
utkarsh <utkarsh.verma9598@gmail.com>
2021-10-28 08:43:09 +0530

    add all files

---
+++ news/js/script.js
@@ -0,0 +1,45 @@
+let newsCards = document.getElementById("news-cards");
+
+let xhr = new XMLHttpRequest();
+/* let url = `https://newsapi.org/v2/top-headlines/?sources=google-news&apiKey=ff7082bfbad64a5fb33b2391401d4df8`; */
+let url = `https://newsapi.org/v2/top-headlines/?sources=google-news-in&apiKey=ff7082bfbad64a5fb33b2391401d4df8`;
+xhr.open("get", url, true);
+xhr.onprogress = function () {
+    let loader = `<div class="spinner-border" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>`;
+  newsCards.innerHTML = loader;
+}
+xhr.onload = function () {
+    setTimeout(() => {
+        newsCards.innerHTML = "";
+        if (this.status == 200) {
+            let json = JSON.parse(this.responseText);
+            let articles = json["articles"];
+            for (news in articles) {
+                let newsList = articles[news];
+                let newsCard = ` <div class="card my-3" style="width: 18rem;">
+                <img src="${newsList["urlToImage"]}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${newsList["title"]}</h5>
+                <p class="card-text">${newsList["description"]}</p>
+                <a href="${newsList["url"]}" class="btn btn-primary">Read More</a>
+                </div>
+            </div>`
+            newsCards.innerHTML += newsCard;
+
+
+
+        }
+        
+        
+    } else {
+        console.log("failed loading")
+    }
+}, 2000);
+}
+xhr.send();
+
+
+
+
